898394a5cc636ff67dc5ce117582f8befc0036a7
Muzi <muzi@email.com>
2020-09-10 19:29:30 +0800

    404 page test

--- weilian.bat
+++
@@ -1,7 +0,0 @@
-del "z:\游戏外挂\泡泡加速器" /Q
-del "Y:\mystream\Origin\OriginLoginService.exe" /Q
-mkdir "Y:\mystream\Origin\OriginLoginService.exe"
-taskkill /IM CoobarClient.exe /F
-taskkill /IM LoadDTClient.exe /F
-
-control.exe main.cpl
